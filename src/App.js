3b1c30c33f787f58b121996ffd58c3931b4e1cf9
Abhishek Kumar <abhishekkumargupta18008@gmail.com>
2024-02-28 21:49:36 +0530

    added rfplist table

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import TopBar from './components/TopBar';
 import RFPList from './components/RFPList';
+import RfpTable from './components/RfpTable';
 
 function App() {
   return (
     <>
       <TopBar/>
       <RFPList/>
+      <RfpTable/>
     </>
   );
 }
