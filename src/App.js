ecf18a3f48cdc24600496113216f98ed98a2e7df
Raghav Agarwal <raghav10022001@gmail.com>
2024-02-28 19:39:05 +0530

    Top Bar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TopBar from './components/TopBar';
+
+function App() {
+  return (
+    <>
+      <TopBar/>
+    </>
+  );
+}
+
+export default App;
