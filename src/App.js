c61c3f6e749452912365a858145f563cce5432a2
Raghav Agarwal <raghav10022001@gmail.com>
2024-02-28 21:07:54 +0530

    Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import TopBar from './components/TopBar';
+import RFPList from './components/RFPList';
 
 function App() {
   return (
     <>
       <TopBar/>
+      <RFPList/>
     </>
   );
 }
---
+++ src/components/RFPList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function RFPList() {
+  return (
+    <div id="midSec">
+      <div id="listLeftSec">
+        <p>Home</p>
+        <h2>RFP List</h2>
+      </div>
+      <div id="listMidSec">
+      </div>
+
+    </div>
+  )
+}
+
+export default RFPList
